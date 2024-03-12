bccf9149e6d0ecf98e72a0fc810bddb67cd73aaf
Hermas Amaewhule <whule@yahoo.com>
2024-03-12 10:33:42 +0000

    updated main.tf file

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
     features {}
-    client_id = "549b492c-2de8-4e83-84fe-d55e63b066bf"
-    client_secret = "KHL8Q~a954lzhfeG.Qqut942LABjem28J_cMwdgY"
-    subscription_id = "8ab233ac-c619-4888-bf7a-422ea867b8d8"
-    tenant_id = "47d4542c-f112-47f4-92c7-a838d8a5e8ef"
+    client_id = var.client_id
+    client_secret = var.client_secret
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
 }
 
 module "network" {
